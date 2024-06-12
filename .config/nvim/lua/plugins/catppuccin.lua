85a262735309be8d09ccc5f4bbd2aff9d2e95679
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-06-12 12:58:04 +0200

    theme integrations

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,2 +1,19 @@
 ---@type LazySpec
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    integrations = {
+      nvimtree = false,
+      notify = true,
+      mason = true,
+      telescope = {
+        enabled = true,
+        style = "",
+      },
+      which_key = true,
+      aerial = true,
+    },
+  },
+}
