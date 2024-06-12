2381436cbf2a864fc33c1b42bbfc9826addebb46
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-06-12 11:04:44 +0200

    proper keybinds, kthxbye o7

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason plugins
 
 ---@type LazySpec
@@ -7,32 +5,27 @@ return {
   -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
       ensure_installed = {
         "lua_ls",
-        -- add more arguments for adding more language servers
       },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
     opts = {
       ensure_installed = {
         "stylua",
-        -- add more arguments for adding more null-ls sources
+        "swiftlint",
       },
     },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
       ensure_installed = {
-        "python",
-        -- add more arguments for adding more debuggers
+        -- "python",
       },
     },
   },
---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,31 @@
+---@type LazySpec
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local maps = opts.mappings
+        maps.n["<Leader>o"] = { "<Cmd>Neotree toggle<CR>", desc = "Toggle Explorer" }
+        maps.n["<Leader>e"] = {
+          function()
+            if vim.bo.filetype == "neo-tree" then
+              vim.cmd.wincmd "p"
+            else
+              vim.cmd.Neotree "focus"
+            end
+          end,
+          desc = "Toggle Explorer Focus",
+        }
+      end,
+    },
+  },
+  opts = {
+    window = {
+      mappings = {
+        ["<S-Tab>"] = "prev_source",
+        ["<Tab>"] = "next_source",
+      },
+    },
+  },
+}
