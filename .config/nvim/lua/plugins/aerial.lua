5590a10732eaa8f9aaefcfd747106ac7473d55a7
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-06-16 22:52:37 +0200

    open symbols automatically if applicant

---
+++ .config/nvim/lua/plugins/aerial.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/aerial.nvim",
+  opts = function()
+    local opts = {
+      open_automatic = function(bufnr)
+        -- Enforce a minimum line count
+        return vim.api.nvim_buf_line_count(bufnr) > 80
+      end,
+      close_automatic_events = { "unsupported" },
+    }
+    return opts
+  end,
+}
