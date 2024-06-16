3be86ad068870f42fac929e63a4bf058aa48c733
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-06-16 19:18:58 +0200

    nvim: cleanup using community lang packs

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,6 +5,13 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.color.headlines-nvim" },
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.swift" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
 }
