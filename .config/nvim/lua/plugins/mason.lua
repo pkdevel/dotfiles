513634ab90cecdce4a35d339132b0a13d8912d9d
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-06-24 21:21:04 +0200

    fix: nvim-dap missing after plugin updates, go.nvim was replaced

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,14 +1,22 @@
--- Customize Mason plugins
-
 ---@type LazySpec
 return {
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = nil,
+    },
+  },
   {
     "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
     opts = {
       ensure_installed = nil,
       automatic_installation = true,
+    },
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = {
+      ensure_installed = nil,
     },
   },
 }
--- .config/nvim/lua/plugins/xcodebuild.lua
+++ .config/nvim/lua/plugins/xcodebuild.lua
@@ -51,6 +51,18 @@ return {
       },
     }
 
+    local xcodebuild = require "xcodebuild.integrations.dap"
+    local codelldbPath = os.getenv "HOME" .. "/.local/share/codelldb"
+    xcodebuild.setup(codelldbPath)
+
+    vim.keymap.set("n", "<leader>dd", xcodebuild.build_and_debug, { desc = "Build & Debug" })
+    vim.keymap.set("n", "<leader>dr", xcodebuild.debug_without_build, { desc = "Debug Without Building" })
+    vim.keymap.set("n", "<leader>dt", xcodebuild.debug_tests, { desc = "Debug Tests" })
+    vim.keymap.set("n", "<leader>dT", xcodebuild.debug_class_tests, { desc = "Debug Class Tests" })
+    vim.keymap.set("n", "<leader>db", xcodebuild.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+    vim.keymap.set("n", "<leader>dm", xcodebuild.toggle_message_breakpoint, { desc = "Toggle Message Breakpoint" })
+    vim.keymap.set("n", "<leader>dx", xcodebuild.terminate_session, { desc = "Terminate Debugger" })
+
     -- stylua: ignore start
     local get_icon = require("astroui").get_icon
     vim.keymap.set("n", "<leader>X", "<cmd>XcodebuildPicker<cr>", { desc = "Show Xcodebuild Actions" })
