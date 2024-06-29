cc0be9152e7ff63b659a1d2d658c8a0d80b590af
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-06-29 08:12:35 +0200

    yabai: dynamic padding ♥

---
+++ yabai/.config/yabai/scripts/padding.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# find total number of windows on current space that are not floating, not sticky, not minimized, but are visible.
+TOTAL="$(yabai -m query --windows --space | jq '.[] | select(."is-floating" == false) | select(."is-sticky" == false) | select(."is-visible" == true) | select(."is-minimized" == false) | length % 23 + 1' | wc -l)"
+
+# find the padding ammount on the current space
+CURRENT=$(yabai -m config --space $(yabai -m query --spaces --space | jq '(."index")') window_gap)
+
+# if the total is greater than 1, set padding, else disable padding.
+if [[ $TOTAL -gt 1 ]]; then
+  if [[ $CURRENT -eq 0 ]]; then
+    yabai -m space --gap abs:15; yabai -m space --padding abs:9:9:9:9
+  fi
+else 
+  if [[ $CURRENT -ne 0 ]]; then
+    yabai -m space --gap abs:0; yabai -m space --padding abs:0:0:0:0
+  fi
+fi
+
