380049b5982b78a3165692aae0f24f578fc68346
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-07-01 12:27:12 +0200

    yabai: remove last sketchybar menu references

--- yabai/.config/sketchybar/items/apple.lua
+++ yabai/.config/sketchybar/items/apple.lua
@@ -2,19 +2,18 @@ local colors = require("colors")
 local icons = require("icons")
 
 sbar.add("item", {
-  icon = {
-    font = { size = 16.0 },
-    string = icons.apple,
-    padding_right = 8,
-    padding_left = 8,
-  },
-  label = { drawing = false },
-  background = {
-    color = colors.bg1,
-    border_color = colors.bg2,
-    border_width = 1,
-  },
-  padding_left = 1,
-  padding_right = 10,
-  click_script = "$CONFIG_DIR/helpers/menus/bin/menus -s 0",
+	icon = {
+		font = { size = 16.0 },
+		string = icons.apple,
+		padding_right = 8,
+		padding_left = 8,
+	},
+	label = { drawing = false },
+	background = {
+		color = colors.bg1,
+		border_color = colors.bg2,
+		border_width = 1,
+	},
+	padding_left = 1,
+	padding_right = 10,
 })
