9c4082913c98fd424ec88c6a5b6a253e051495dc
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-07-02 09:11:56 +0200

    yabai: optimized padding script

--- yabai/.config/yabai/scripts/padding.sh
+++ yabai/.config/yabai/scripts/padding.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # find total number of windows on current space that are not floating, not sticky, not minimized, but are visible.
-TOTAL="$(yabai -m query --windows --space | jq '.[] | select(."is-floating" == false) | select(."is-sticky" == false) | select(."is-visible" == true) | select(."is-minimized" == false) | length % 23 + 1' | wc -l)"
+TOTAL="$(yabai -m query --windows --space | jq '.[] | select(."is-floating" == false) | select(."is-sticky" == false) | select(."is-visible" == true) | select(."is-minimized" == false)' | jq --slurp '. | length')"
 
 # find the padding ammount on the current space
 CURRENT=$(yabai -m config --space $(yabai -m query --spaces --space | jq '(."index")') window_gap)
