30d77822e4328320c16a6a76c25d4da9bafa0d72
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-07-13 13:32:27 +0200

    neovim: starting out with new basic lazy config

---
+++ nvim.lazy/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "PREV Buffer" })
+map("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "<C-q>", LazyVim.ui.bufremove, { desc = "Next Buffer" })
---
+++ nvim.lazy/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      mappings = {
+        ["<S-Tab>"] = "prev_source",
+        ["<Tab>"] = "next_source",
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        visible = true, -- when true, they will just be displayed differently than normal items
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+  },
+}
