49c43a326398b94cc8a3acb51beacecee6370c48
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-07-13 13:34:11 +0200

    neovim: adding some plugins

---
+++ nvim.lazy/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,38 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+    "nvim-telescope/telescope.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "ObsidianVault",
+        path = "~/Documents/ObsidianVault",
+      },
+      {
+        name = "dotfiles",
+        path = "~/dotfiles",
+      },
+    },
+    use_advanced_uri = true,
+    finder = "telescope.nvim",
+
+    note_frontmatter_func = function(note)
+      -- This is equivalent to the default frontmatter function.
+      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+      return out
+    end,
+  },
+}
