9ce59f5e9228a2333d71b40f8dc51fe3335f0f41
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-07-15 18:28:21 +0200

    nvim: disable netrw rtp

--- nvim.lazy/.config/nvim/lua/config/lazy.lua
+++ nvim.lazy/.config/nvim/lua/config/lazy.lua
@@ -52,7 +52,7 @@ require("lazy").setup({
         "gzip",
         -- "matchit",
         -- "matchparen",
-        -- "netrwPlugin",
+        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
