98942f4d84fab5c6c56b79f321b8e042aa5f08fb
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-07-15 18:27:44 +0200

    nvim: config cleanup

--- nvim.lazy/.config/nvim/lua/plugins/xcodebuild.lua
+++ nvim.lazy/.config/nvim/lua/plugins/xcodebuild.lua
@@ -1,5 +1,3 @@
-local progress_handle
-
 return {
   {
     "wojciech-kulik/xcodebuild.nvim",
@@ -19,32 +17,6 @@ return {
           auto_focus = false,
           auto_close_on_app_launch = true,
           only_summary = true,
-          -- notify = function(message, severity)
-          --   local fidget = require "fidget"
-          --   if progress_handle then
-          --     progress_handle.message = message
-          --     if not message:find "Loading" then
-          --       progress_handle:finish()
-          --       progress_handle = nil
-          --       if vim.trim(message) ~= "" then fidget.notify(message, severity) end
-          --     end
-          --   else
-          --     fidget.notify(message, severity)
-          --   end
-          -- end,
-          -- notify_progress = function(message)
-          --   local progress = require "fidget.progress"
-          --
-          --   if progress_handle then
-          --     progress_handle.title = ""
-          --     progress_handle.message = message
-          --   else
-          --     progress_handle = progress.handle.create {
-          --       message = message,
-          --       lsp_client = { name = "xcodebuild.nvim" },
-          --     }
-          --   end
-          -- end,
         },
         code_coverage = {
           enabled = true,
