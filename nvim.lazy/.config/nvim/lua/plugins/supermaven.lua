f3fe2344e533fabddb1a6c248d0ea99d19964316
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-07-16 17:03:28 +0200

    nvim: lazy config

--- nvim.lazy/.config/nvim/lua/plugins/supermaven.lua
+++ nvim.lazy/.config/nvim/lua/plugins/supermaven.lua
@@ -14,6 +14,7 @@ return {
   },
   {
     "supermaven-inc/supermaven-nvim",
+    lazy = false,
     config = function()
       require("supermaven-nvim").setup({
         color = {
--- nvim.lazy/.config/nvim/lua/plugins/tokyonight.lua
+++ nvim.lazy/.config/nvim/lua/plugins/tokyonight.lua
@@ -1,5 +1,6 @@
 return {
   "folke/tokyonight.nvim",
+  priority = 1000,
   opts = {
     style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
     light_style = "day", -- The theme is used when the background is set to light
