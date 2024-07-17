b39bf995fc064a7a019b7bc9441546afd0315358
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-07-17 09:05:12 +0200

    nvim: remove supermaven source from cmp

--- nvim.lazy/.config/nvim/lua/plugins/supermaven.lua
+++ nvim.lazy/.config/nvim/lua/plugins/supermaven.lua
@@ -1,30 +1,15 @@
 return {
-  {
-    "nvim-cmp",
-    dependencies = {
-      "supermaven-inc/supermaven-nvim",
-    },
-    opts = function(_, opts)
-      table.insert(opts.sources, {
-        name = "supermaven",
-        group_index = 1,
-        priority = 100,
-      })
-    end,
-  },
-  {
-    "supermaven-inc/supermaven-nvim",
-    lazy = false,
-    config = function()
-      require("supermaven-nvim").setup({
-        keymaps = {
-          accept_suggestion = "<S-Tab>",
-        },
-        color = {
-          suggestion_color = "#efc777",
-          cterm = 117,
-        },
-      })
-    end,
-  },
+  "supermaven-inc/supermaven-nvim",
+  lazy = false,
+  config = function()
+    require("supermaven-nvim").setup({
+      keymaps = {
+        accept_suggestion = "<S-Tab>",
+      },
+      color = {
+        suggestion_color = "#efc777",
+        cterm = 117,
+      },
+    })
+  end,
 }
