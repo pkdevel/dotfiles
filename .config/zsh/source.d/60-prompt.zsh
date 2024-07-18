17d2a67763e66d579a294cc6c7e25094938c3337
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-07-18 17:18:13 +0200

    zsh: load pure prompt on ssh connection

--- .config/zsh/source.d/60-prompt.zsh
+++ .config/zsh/source.d/60-prompt.zsh
@@ -1,7 +1,9 @@
 if [[ -v OH_MY_POSH ]]; then
   eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/atomic.omp.json)"
-else
-  # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+elif [ -z "$SSH_CLIENT" ] || [ -z "$SSH_TTY" ]; then
   zi depth=1 lucid for romkatv/powerlevel10k
   [[ ! -f $ZDOTDIR/.p10k.zsh ]] || source $ZDOTDIR/.p10k.zsh
+else
+  zinit ice pick"async.zsh" src"pure.zsh"
+  zinit light sindresorhus/pure
 fi
