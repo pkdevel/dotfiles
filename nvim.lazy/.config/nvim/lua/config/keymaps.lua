357899ed7738af26bb0d0ef1c8fdbacc91e9e51c
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-08-06 12:48:04 +0200

    fix(typo)

--- nvim.lazy/.config/nvim/lua/config/keymaps.lua
+++ nvim.lazy/.config/nvim/lua/config/keymaps.lua
@@ -3,8 +3,8 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 
-map("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "PREV Buffer" })
 map("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-map("n", "<C-q>", LazyVim.ui.bufremove, { desc = "Next Buffer" })
+map("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<C-q>", LazyVim.ui.bufremove, { desc = "Close Buffer" })
 
 map({ "n", "i" }, "<M-z>", "<cmd>ZenMode<cr>", { desc = "Zen mode" })
