afd0a11099766bcfdc88cc80807592b2b594dffd
Philipp Kolb <pkdevel@users.noreply.github.com>
2024-08-26 15:03:16 +0200

    feat(ai): disable copilot

--- nvim.lazy/.config/nvim/lua/config/lazy.lua
+++ nvim.lazy/.config/nvim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import extra plugins
-    { "nvim-cmp", keys = false },
+    -- { "nvim-cmp", keys = false },
     { import = "lazyvim.plugins.extras.editor.refactoring" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.docker" },
