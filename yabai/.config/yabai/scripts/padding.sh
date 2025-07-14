8d19eb42c59f2ecabb5924b251318737332acb4c
Philipp Kolb <pkdevel@users.noreply.github.com>
2025-07-14 08:56:11 +0200

    feat(yabai): using auto padding script

--- yabai/.config/yabai/scripts/padding.sh
+++ yabai/.config/yabai/scripts/padding.sh
@@ -9,11 +9,12 @@ CURRENT=$(yabai -m config --space $(yabai -m query --spaces --space | jq '(."ind
 # if the total is greater than 1, set padding, else disable padding.
 if [[ $TOTAL -gt 1 ]]; then
   if [[ $CURRENT -eq 0 ]]; then
-    yabai -m space --gap abs:20; yabai -m space --padding abs:20:16:18:18
+    yabai -m space --gap abs:20
+    yabai -m space --padding abs:20:16:18:18
   fi
-else 
+else
   if [[ $CURRENT -ne 0 ]]; then
-    yabai -m space --gap abs:0; yabai -m space --padding abs:0:0:0:0
+    yabai -m space --gap abs:0
+    yabai -m space --padding abs:0:0:0:0
   fi
 fi
-
