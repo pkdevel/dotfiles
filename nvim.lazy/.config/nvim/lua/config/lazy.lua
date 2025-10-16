e48bf90454c9c24a552241c84f58f8cca5073a26
Philipp Kolb <pkdevel@users.noreply.github.com>
2025-10-16 16:06:32 +0200

    chore(lazy): update from starter

--- nvim.lazy/.config/nvim/lua/config/lazy.lua
+++ nvim.lazy/.config/nvim/lua/config/lazy.lua
@@ -62,7 +62,7 @@ require("lazy").setup({
         "gzip",
         -- "matchit",
         -- "matchparen",
-        "netrwPlugin",
+        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
